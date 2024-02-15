7e575dc7298d5c9b12ba201c6756eb97a029d71d
Konstantin <humbl3dev@gmail.com>
2024-02-14 20:54:58 -0800

    avatar and conditional link for admin route

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import { Role } from '@prisma/client';
 	$: isHomePage = $page.route.id === '/';
 	$: console.log($page.data);
 </script>
@@ -7,13 +8,16 @@
 <div class="absolute right-0 top-0 z-50 w-full p-4 {isHomePage ? 'text-white' : ''}">
 	<div class="mx-auto flex max-w-screen-2xl items-center justify-between">
 		<a href="/" class="text-2xl font-bold">Vivid Ventures</a>
-		<nav class="flex gap-6">
-			<a href="/" class="link text-lg no-underline">Shop</a>
-			<a href="/about" class="link text-lg no-underline">About</a>
+		<nav class="flex gap-6 text-base">
+			<a href="/" class="link no-underline">Shop</a>
+			<a href="/about" class="link no-underline">About</a>
 		</nav>
 		{#if $page.data.username}
 			<div class="flex items-center gap-2">
-				<a href="/user" class="btn btn-outline btn-primary btn-sm inline-flex items-center gap-2">
+				<a
+					href={$page.data.role === Role.ADMIN ? '/admin' : '/user'}
+					class="btn btn-ghost btn-sm inline-flex items-center gap-2"
+				>
 					{#if $page.data.avatar}
 						<img class="max-w-6 object-contain" src={$page.data.avatar} alt={$page.data.username} />
 						<span>{$page.data.username}</span>
@@ -22,14 +26,13 @@
 					{/if}
 				</a>
 				<form method="post" action="/sign-out">
-					<button type="submit" class="btn btn-outline btn-sm text-neutral-content">Sign Out</button
-					>
+					<button type="submit" class="btn btn-outline btn-error btn-sm">Sign Out</button>
 				</form>
 			</div>
 		{:else}
-			<div>
-				<a href="/login" class="btn btn-accent btn-sm">Login</a>
-				<a href="/sign-up" class="btn btn-ghost btn-sm">Sign Up</a>
+			<div class="flex gap-2">
+				<a href="/login" class="btn btn-sm">Login</a>
+				<a href="/sign-up" class="btn btn-primary btn-sm">Sign Up</a>
 			</div>
 		{/if}
 	</div>
--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,6 +3,7 @@ export const load = (event) => {
 
 	return {
 		username: event.locals.user?.username,
-		avatar: event.locals.user?.avatar
+		avatar: event.locals.user?.avatar,
+		role: event.locals.user?.role
 	};
 };
