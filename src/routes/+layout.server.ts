283a163235fc5190ef2d86c803da73e9adb12cf2
Konstantin <humbl3dev@gmail.com>
2024-04-09 09:58:17 -0700

    add cart count on every page load

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,25 @@
-export const load = (event) => {
+import prisma from '$lib/prisma';
+
+export const load = async (event) => {
+	const userId = event.locals.user?.id;
+	let cartItemsCount = 0;
+
+	if (userId) {
+		const cart = await prisma.cart.findUnique({
+			where: {
+				userId
+			},
+			include: {
+				items: true
+			}
+		});
+		cartItemsCount = cart?.items.length ?? 0;
+	}
+
 	return {
 		username: event.locals.user?.username,
 		avatar: event.locals.user?.avatar,
-		role: event.locals.user?.role
+		role: event.locals.user?.role,
+		cartItemsCount
 	};
 };
